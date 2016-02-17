9cad247b5d0309fa673068693d288ba7e07c0e95
Ruy Adorno <ruyadorno@hotmail.com>
2016-02-17 17:13:42 -0500

    Removed -n parameter from gallery commands

--- gallery.sh
+++ gallery.sh
@@ -1,10 +1,10 @@
 
 # irm: Selects files to delete from current folder (recommended to use trash instead of rm -rf)
-alias irm="ls | ipt -m -n | xargs rm -rf"
+alias irm="ls | ipt -m | xargs rm -rf"
 
 # irebase: Interactive build a list of git commits from log and rebase from selected one
-alias irebase="git --no-pager log --oneline | ipt -n | cut -d ' ' -f 1 | xargs -o git rebase -i"
+alias irebase="git --no-pager log --oneline | ipt | cut -d ' ' -f 1 | xargs -o git rebase -i"
 
 # icheckout: Interactive git checkout a commit, similar to irebase
-alias icheckout="git --no-pager log --oneline | ipt -n | cut -d ' ' -f 1 | xargs git checkout"
+alias icheckout="git --no-pager log --oneline | ipt | cut -d ' ' -f 1 | xargs git checkout"
 
